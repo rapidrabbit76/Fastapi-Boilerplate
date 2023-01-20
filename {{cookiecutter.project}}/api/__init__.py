094a403d7997ea570b697705e8e34d3bd580628f
yslee <yslee.dev@gmail.com>
2023-01-20 15:25:01 +0900

    fix: api routers

--- {{cookiecutter.project}}/api/__init__.py
+++ {{cookiecutter.project}}/api/__init__.py
@@ -1,2 +1,18 @@
-from .home import router
+from datetime import datetime
+from fastapi import (
+    APIRouter,
+    Response,
+)
 
+router = APIRouter()
+
+
+async def index():
+    """ALB check"""
+    current_time = datetime.utcnow()
+    msg = f"Notification API (UTC: {current_time.strftime('%Y.%m.%d %H:%M:%S')})"
+    return Response(msg)
+
+
+router.add_api_route("/", index, status_code=200, methods=["GET"])
+router.add_api_route("/health", index, status_code=200, methods=["GET"])
